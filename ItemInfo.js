463f6e29675d873593e3ddd087ff2af065306774
ArceusMatt <arceusthegod303@yahoo.com>
2016-01-31 02:11:23 -0500

    Create ItemInfo.js

---
+++ ItemInfo.js
@@ -0,0 +1,5 @@
+function newLevel(){
+clientMessage("Item ID: " + Player.getCarriedItem());
+clientMessage("Amount in hand: " + Player.getCarriedItemCount());
+clientMessage("Data / damage: " + Player.getCarriedItemData());
+}
