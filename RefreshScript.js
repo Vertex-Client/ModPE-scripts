061e2b1ddd83098cbd825d9c6d456d80be4dbf5c
ArceusMatt <arceusthegod303@yahoo.com>
2016-01-31 02:02:47 -0500

    Create RefreshScript.js

---
+++ RefreshScript.js
@@ -0,0 +1,7 @@
+var modpeFolder = ctx.getDir("ModPE", 0);
+var modpeFile = new java.io.File(modpeFolder, "ModName.js");
+
+function modTick(){
+net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(ModName.js, true);
+net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(ModName.js, false);
+}
