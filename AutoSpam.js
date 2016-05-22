e61c45e4c7910349259590ea6e566a9c2203a396
ArceusMatt <arceusthegod303@yahoo.com>
2016-05-22 19:25:03 -0400

    Create AutoSpam.js

---
+++ AutoSpam.js
@@ -0,0 +1,9 @@
+var spam = false;
+
+function newLevel() {
+spam = true
+}
+
+function modTick() {
+if(spam)net.zhuoweizhang.mcpelauncher.ScriptManager.nativeSendChat("SPAM SPAM SPAM\nSpam SPam SPAM\nSPAM SPAM spam!");
+}
