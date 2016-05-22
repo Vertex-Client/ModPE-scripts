2182753901d108e6c9cdbd00164d349ba8061493
ArceusMatt <arceusthegod303@yahoo.com>
2016-05-22 19:31:26 -0400

    Update ItemInfo.js

--- ItemInfo.js
+++ ItemInfo.js
@@ -1,5 +1,11 @@
-function newLevel(){
+var iteminfo = false;
+
+if(iteminfo) {
 clientMessage("Item ID: " + Player.getCarriedItem());
 clientMessage("Amount in hand: " + Player.getCarriedItemCount());
 clientMessage("Data / damage: " + Player.getCarriedItemData());
+}
+
+function chatReceiveHook(str, sender) {
+	if(str=="ItemInfo")iteminfo = true;
 }
