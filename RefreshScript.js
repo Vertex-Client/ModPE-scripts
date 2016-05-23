cf1361932388c5a8d91362d39c5cd73c8b35ab24
ArceusMatt <arceusthegod303@yahoo.com>
2016-05-22 20:22:27 -0400

    Update RefreshScript.js

--- RefreshScript.js
+++ RefreshScript.js
@@ -1,7 +1,13 @@
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
 var modpeFolder = ctx.getDir("ModPE", 0);
 var modpeFile = new java.io.File(modpeFolder, "ModName.js");
 
-function modTick(){
+var start = false;
+
+if(start)refresh();
+
+function refresh(){
 net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(ModName.js, true);
 net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(ModName.js, false);
 }
