89f6100ad44de3cc7143bbebde20b33349a5a58f
ArceusMatt <arceusthegod303@yahoo.com>
2016-05-22 20:18:03 -0400

    Create ClearChat.js

---
+++ ClearChat.js
@@ -0,0 +1,17 @@
+function chatReceiveHook(str, sender) {
+	if(str=="clear") {
+	clientMessage(" " + "\n" + " ");
+
+clientMessage(" " + "\n" + " ");
+
+clientMessage(" " + "\n" + " ");
+
+clientMessage(" " + "\n" + " ");
+
+clientMessage(" " + "\n" + " ");
+
+clientMessage(" " + "\n" + " ");
+
+clientMessage(" " + "\n" + " ");
+}
+	}
