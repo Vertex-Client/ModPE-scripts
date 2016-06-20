6c5405ee6b6513fcd8be430460d12f360ab8a12e
ArceusMatt <arceusthegod303@yahoo.com>
2016-06-20 09:54:41 -0400

    Fixed typo, thanks @Vertex-Client

--- Tap destory.js
+++ Tap destory.js
@@ -1,17 +1,17 @@
-var destory = false;
+var destroy = false;
 var option = false;
 
 function chatReceiveHook(str, sender) {
 if(str=="Drop")option = true;
 if(str=="NoDrop")option = false;
 	if(str=="Go") {
-	destory = true;
+	destroy = true;
 	clientMessage("Tap ground.")
 	}
 }
 
 function useItem(x, y, z, itemId, blockId, side){
-if(destory){
+if(destroy){
 	Level.destroyBlock(x +1, y, z +1, option);
 	Level.destroyBlock(x +2, y, z +2, option);
 	Level.destroyBlock(x +3, y, z +3, option);
